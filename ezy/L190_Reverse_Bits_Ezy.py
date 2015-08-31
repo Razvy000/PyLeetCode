c0b571696bf2a96fd3eea7de3866301497a04483
Razvy000 <razvy000@gmail.com>
2015-08-31 10:49:13 +0100

    first

---
+++ ezy/L190_Reverse_Bits_Ezy.py
@@ -0,0 +1,26 @@
+'''
+Reverse bits of a given 32 bits unsigned integer.
+
+For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+Follow up:
+If this function is called many times, how would you optimize it?
+'''
+
+class L190_Reverse_Bits_Ezy:
+    
+    # @param n, an integer
+    # @return an integer
+    def reverseBits2(self, n):
+        oribin = '{0:032b}'.format(n)
+        reversebin = oribin[::-1]
+        return int(reversebin, 2)
+        
+    # O(log(n))
+    def reverseBits(self, n):
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
+        return n
